32df7f9874a25bc5f4106827f767ea3b8aec38cf
Kauha-Costa <kauhaerickcosta@gmail.com>
2025-03-17 15:03:39 -0300

    Category

---
+++ src/main/java/com/projetocursojava/course/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.projetocursojava.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projetocursojava.course.entities.Category;
+import com.projetocursojava.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+}
